4392fba0748935b4ccce4e95f996dbff3d7f0283
YGMR_YT <91434018+YGameRRYT@users.noreply.github.com>
2023-02-14 21:55:35 -0800

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,13 @@
-eventTarget.addEventListener("keydown", (event) => {
-  if (event.isComposing || event.keyCode === 13) {
-    return;
+// Get the input field
+var input = document.getElementById("myInput");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("click").bakeCookie();
   }
-  bakeCookie();
 });
